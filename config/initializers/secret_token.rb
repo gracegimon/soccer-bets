6606635e3aba2c0aa8cb35ae313db5239e09fefe
Grace Gimon <gracegimon@gmail.com>
2014-01-21 21:58:07 -0430

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quiniela::Application.config.secret_key_base = 'eafed6972103e96c49f0af5b0553c13fd6ef8a34be35b1f9ef2fb3ee4f15be88f83443697092b6a1cf12f3d8ed0f9f554c938ae52e739ff692cf7aefabe8afd8'
