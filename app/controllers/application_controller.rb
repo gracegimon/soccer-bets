22d54e163e5c99e015ef03853a4ebe9200a28447
Grace Gimon <gracegimon@gmail.com>
2014-01-26 14:37:55 -0430

    Show action secured

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,12 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
 
+  def current_user?(user)
+    current_user == user
+  end
+
   def authenticate
-    redirect_to :root unless current_user.nil?
+    redirect_to :root unless @current_user.nil?
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
-  before_filter :authenticate 
+  before_action :signed_in, only: [:show]
   def show
     @user = User.find(params[:id])
   end
+
+  private
+
+  def signed_in
+	@user = User.find(params[:id])
+	redirect_to(root_url) unless current_user?(@user)
+  end
+
 end
