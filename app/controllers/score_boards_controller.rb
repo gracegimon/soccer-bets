34758c16496425d7f29915706e5d0ca9f426b01f
Grace Gimon <gracegimon@gmail.com>
2014-03-15 17:36:56 -0430

    Verified permissions of viewing

--- app/controllers/score_boards_controller.rb
+++ app/controllers/score_boards_controller.rb
@@ -1,7 +1,9 @@
 class ScoreBoardsController < ApplicationController
-  before_filter :authenticate
+ #  before_filter :authenticate
   before_action :check_admin, only: [:tournament_score_board, :show_for_admin]
   before_action :check_published, only: [:show_after_published]
+  before_action :current_user_is_viewing, except: [:show_after_published]
+  before_action :signed_in, only: [:show_after_published]
 
   def index
     @score_boards = current_user.score_boards
@@ -197,6 +199,16 @@ class ScoreBoardsController < ApplicationController
   def check_published
     is_published = ScoreBoard.find(params[:id]).is_published?
     return true if is_published
+  end
+
+  def current_user_is_viewing
+    @user = ScoreBoard.find(params[:id]).user
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def signed_in
+    @user = ScoreBoard.find(params[:id]).user
+    redirect_to(root_url) unless (current_user?(@user) || current_user.is_admin?)
   end
 
 end
