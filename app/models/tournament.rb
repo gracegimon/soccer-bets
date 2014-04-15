5e8374d50324d06b5bb7dbeca52106a8f570f9fa
Grace Gimon <gracegimon@gmail.com>
2014-04-14 21:41:23 -0430

    Added welcome index new information

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -6,31 +6,35 @@ class Tournament < ActiveRecord::Base
   scope :active, -> {where is_active: 1}
 
   # Returns the current value of the jackpot
-  def jackpot
-  	score_boards_type_0 = ScoreBoard.active.where(board_type: ScoreBoard::GENERAL).count
-  	score_boards_type_1 = ScoreBoard.active.where(board_type: ScoreBoard::HIGH_ROLLER).count
-  	return (score_boards_type_0 * ScoreBoard::GENERAL_VALUE) + 
-  			(score_boards_type_1 * ScoreBoard::HIGH_ROLLER_VALUE)
+  def jackpot_general
+  	score_boards_type_0 = ScoreBoard.active.where(board_type: ScoreBoard::GENERAL, is_active: true).count
+
+  	return (score_boards_type_0 * ScoreBoard::GENERAL_VALUE)
+  end
+
+  def jackpot_high
+    score_boards_type_1 = ScoreBoard.active.where(board_type: ScoreBoard::HIGH_ROLLER, is_active: true).count
+    return (score_boards_type_1 * ScoreBoard::HIGH_ROLLER_VALUE)  
   end
 
   # First place current prize (50%)
-  def first_place
+  def first_place(jackpot)
   	first_place = jackpot
   	return first_place * 0.5
   end
 
-  def second_place
+  def second_place(jackpot)
   	second_place = jackpot
   	return second_place * 0.2
   end
 
-  def third_place
+  def third_place(jackpot)
   	third_place = jackpot
   	return third_place * 0.1
   end
 
   # Returns the 80% of the jackpot
-  def final_jackpot
+  def final_jackpot(jackpot)
   	final_jackpot = jackpot
   	return final_jackpot * 0.8
   end
