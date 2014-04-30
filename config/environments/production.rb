4b17c2bc0896ebf123ec673b21b11ee4aa8ce39b
Grace Gimon <gracegimon@gmail.com>
2014-04-29 20:36:06 -0430

    Fixed typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Quiniela::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  cconfig.assets.precompile += ["*jQueryUI/*"]
+  config.assets.precompile += ["*jQueryUI/*"]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
