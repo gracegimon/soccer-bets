d7bd63619eae0bcbb41f7533a8d4cff885820432
Grace Gimon <gracegimon@gmail.com>
2014-05-01 10:44:27 -0430

    Changed the way of adding group scores to avoid deadlock

--- app/controllers/team_stats_controller.rb
+++ app/controllers/team_stats_controller.rb
@@ -12,7 +12,7 @@ class TeamStatsController < ApplicationController
     match_type = Match::R16
     @is_main = @score_board.is_main?
     if @is_main
-    	match_type = Match::R16_MAIN
+      match_type = Match::R16_MAIN
     end
     # up
     @matches = Match.where(match_type: match_type, score_board_id: @score_board.id).order(:match_number)
@@ -21,5 +21,17 @@ class TeamStatsController < ApplicationController
     else
       @matches = @score_board.update_round_of_16(@matches)
     end
+  end
+
+  def update_team_stats_for_group
+    @group = Group.find(params[:group_id])
+    @score_board = ScoreBoard.find(params[:score_board_id])
+    @teams = @group.teams
+    @teams.each do |team|
+      team.read_team_stats(@score_board)
+    end
+    leaders = @group.group_leaders_for_score_board(@score_board)
+    bottom = @group.group_bottom_for_score_board(@score_board)
+    Score.set_group_positions(leaders,bottom,@score_board)
   end
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -15,44 +15,44 @@ class Score < ActiveRecord::Base
 #  validates :winner_team_id, presence: true
 
   def match
-  	Match.find(self.match_id)
+    Match.find(self.match_id)
   end
 
   def score_board
-  	ScoreBoard.find(self.scoreboard_id)
+    ScoreBoard.find(self.scoreboard_id)
   end
 
   def set_winner
     unless team_1_goals.nil? || team_2_goals.nil?
-    	if team_1_goals > team_2_goals
-    		self.winner_team_id = self.match.team_1_id
-    	elsif team_2_goals > team_1_goals
-    		self.winner_team_id = self.match.team_2_id
-    	else
-    		self.winner_team_id = 0 # This means tie
-    	end
+      if team_1_goals > team_2_goals
+        self.winner_team_id = self.match.team_1_id
+      elsif team_2_goals > team_1_goals
+        self.winner_team_id = self.match.team_2_id
+      else
+        self.winner_team_id = 0 # This means tie
+      end
     end
   end
 
   def update_match_stats
-    if self.match.match_type == Match::GROUP_USERS || self.match.match_type == Match::GROUP_MAIN
+    if self.match.match_type == Match::GROUP_MAIN
       self.match.update_teams_stats
       leaders = self.match.team_1.group.group.group_leaders_for_score_board(self.score_board)
       bottom = self.match.team_1.group.group.group_bottom_for_score_board(self.score_board)
-      set_group_positions(leaders,bottom)
+      Score.set_group_positions(leaders,bottom, self.score_board)
     end
 
   end
 
-  def set_group_positions(leaders,bottom)
-    team_s_1 = leaders.first.team_stats.for_scoreboard(self.score_board)
+  def self.set_group_positions(leaders,bottom, score_board)
+    team_s_1 = leaders.first.team_stats.for_scoreboard(score_board)
     team_s_1.set_position(1)
-    team_s_2 = leaders.second.team_stats.for_scoreboard(self.score_board)
+    team_s_2 = leaders.second.team_stats.for_scoreboard(score_board)
     team_s_2.set_position(2)
     team_s_1.save
     team_s_2.save
-    team_s_3 = bottom.first.team_stats.for_scoreboard(self.score_board)
-    team_s_4 = bottom.second.team_stats.for_scoreboard(self.score_board)
+    team_s_3 = bottom.first.team_stats.for_scoreboard(score_board)
+    team_s_4 = bottom.second.team_stats.for_scoreboard(score_board)
     team_s_3.set_position(3)
     team_s_3.save
     team_s_4.set_position(4)
@@ -249,4 +249,3 @@ class Score < ActiveRecord::Base
 
 
 end
-
