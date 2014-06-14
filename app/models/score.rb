e713f559ed24f33dfd7b7374410f79b6193c7033
Grace Gimon <gracegimon@gmail.com>
2014-06-14 12:00:16 -0430

    Added changes for calculation

--- app/models/score.rb
+++ app/models/score.rb
@@ -80,10 +80,9 @@ class Score < ActiveRecord::Base
       match_type = self.match.match_type
       if match_type == Match::GROUP_MAIN
         score_boards.each do |score_board|
-          score_board.points = 0
-          score_board.save
-          matches = select_group_matches(score_board)
-          update_each_group_match(matches, score_board)
+          #  matches = select_group_matches(score_board)
+          # update_each_group_match(matches, score_board)
+          update_points_for_match(self.match, score_board)
         end
       elsif match_type == Match::THIRD_MAIN
         score_boards.each do |score_board|
@@ -166,6 +165,7 @@ class Score < ActiveRecord::Base
           score_board.save
           matches = select_group_matches(score_board)
           update_each_group_match(matches, score_board)
+          self.score_board.extra_phase.set_point_for_score_board(score_board)
         end    
       elsif match_type == Match::THIRD_MAIN
         @score_boards = ScoreBoard.not_main_board.active
@@ -194,6 +194,16 @@ class Score < ActiveRecord::Base
         end        
         # check if winner (9) and sub champion (8)
       end
+    end
+  end
+
+  # JUST A PARTICULAR MATCH
+  def update_points_for_match(match, score_board)
+    main_score_board = ScoreBoard.where(tournament_id: Tournament.where(is_active: true).first.id, user_id: nil).first
+    official_match = match
+    official_score = official_match.score
+    unless official_score.nil?
+      score_board.set_points_for_group_phase(official_score, official_match.match_number)
     end
   end
 
